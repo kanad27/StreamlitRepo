27942e2141dd7489875f69965eaf7d2a5c729500
kanad27 <kanad27@gmail.com>
2023-10-10 14:46:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import tensorflow as tf
 
 st.balloons()
 
@@ -15,3 +16,20 @@ with st.container():
     
     color = st.color_picker('Pick A Color', '#00f900')
     st.write('The current color is', color)
+
+
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
+    bytes_data = img_file_buffer.getvalue()
+    img_tensor = tf.io.decode_image(bytes_data, channels=3)
+
+    # Check the type of img_tensor:
+    # Should output: <class 'tensorflow.python.framework.ops.EagerTensor'>
+    st.write(type(img_tensor))
+
+    # Check the shape of img_tensor:
+    # Should output shape: (height, width, channels)
+    st.write(img_tensor.shape)
