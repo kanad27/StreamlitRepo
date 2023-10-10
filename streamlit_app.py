53551b7526473e133064eaa9c94ed79c77d112a8
kanad27 <kanad27@gmail.com>
2023-10-10 13:42:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ st.info('This is a Streamlit Test Application', icon="ℹ️")
 
 with st.echo():
     st.write('This is how text is displayed in Streamlit')
+
+title = st.text_input('AppSec', 'Database Security and Authentication Management')
+st.write('We are working in POD', title)
